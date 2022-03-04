01b8cca183e08fd951bf5b6c9163dfb51dcf1d2f
marcexl <mxlgallardo@gmail.com>
2022-03-04 12:14:35 -0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-var cursor = document.getElementById("cursor");
+/*
+ * mixed by Marcexl
+ * version 03032022
+ */ 
+
+var cursor    = document.getElementById("cursor");
+var h         = 0;
+var hamburger = $('#hamburger-icon');
+var logo      = $('.navbar-brand img');
+
 document.onmousemove = function(e){
    cursor.style.left = (e.pageX - 25) + "px";
    cursor.style.top = (e.pageY - 25) + "px";
@@ -6,10 +15,9 @@ document.onmousemove = function(e){
 }
 
 $( document ).ready(function() {
-    var h = 0;
-    var hamburger = $('#hamburger-icon');
-    hamburger.click(function() {
-      if(h == 0){
+     hamburger.click(function() {
+      if(h == 0)
+      {
          hamburger.toggleClass('active');
          $("#sidenav-1").css("margin-left","0");
          menuAnimation(h);
@@ -22,8 +30,40 @@ $( document ).ready(function() {
          h = 0;
          return false;
       } 
-    });
+   });
+ 
+   document.getElementById('myVideo').play();
+   document.getElementById('myVideo').addEventListener('ended',myHandler,false);
+   function myHandler(e) {
+      startGame(); 
+   }
 });
+
+
+function startGame(){
+   let elem = document.getElementById("main");
+   elem.style.opacity = 0;
+   setTimeout(function(){
+      $("#proyectos").css("display","block");
+      logoShow()
+   },1000);
+}
+
+function logoShow(){
+   setTimeout(function(){
+      logo.css("margin-top","0");
+      logo.css("opacity","1");
+      hamburguerShow()
+   },300);
+}
+
+function hamburguerShow(){
+   setTimeout(function(){
+      hamburger.css("transform","scale(1)");
+      hamburger.css("opacity","1");
+   },300);
+   sectionOpen(1)
+}
 
 function menuAnimation(h){
    let cnt = 1;
@@ -48,3 +88,23 @@ function menuAnimation(h){
       cnt = 1;
    }
 }
+
+function sectionOpen(id){
+   hamburger.removeClass('active');
+   $("#sidenav-1").css("margin-left","-100%");
+   menuAnimation(h);
+   h = 0;
+
+  const section = ["","proyectos","somos","clientes","blog","contacto"];
+
+  $(".section").addClass("hidesection");
+   setTimeout(function(){
+      $("#"+section[id]).removeClass("hidesection"); 
+      $("#"+section[id]).css("display","block"); 
+      if(section[id] == 'proyectos'){
+         $("#proyectos").css("margin-left","0");
+      }
+   },500);
+}
+
+
